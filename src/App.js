b917cae8a4d67d285a454af029366e4c6de02eaa
Asidipta Chaudhuri <asidiptachaudhuri@gmail.com>
2020-10-19 00:42:34 +0530

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import api from './services/api';
 import SearchBar from './components/SearchBar';
 import Summary from './components/Summary/';
 
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await api.get('countries');
+      setCountries(response.data);
+    };
+
+    fetchData();
+  }, [])
+
   return (
     <div className="App">
-      <SearchBar />
+      <SearchBar countries={countries}/>
       <Summary />
     </div>
   );
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -4,16 +4,16 @@ import api from '../../services/api';
 
 import './searchBar.css';
 
-export default function SearchBar() {
+export default function SearchBar({ countries }) {
   const [searchTerm, setSearchTerm] = useState('');
+  const [detailedData, setDetailedData] = useState([]);
 
   useEffect(() => {
-    const fetchData = (search) => {
+    const fetchData = async (search) => {
       if (search) {
-        const query = search.toLowerCase();
         const date = new Date().toISOString();
-        const response = api.get(`country/${query}?from=2020-03-01T00:00:00Z&to=${date}`);
-        console.log(response['Country']);
+        const response = await api.get(`country/${search}?from=2020-03-01T00:00:00Z&to=${date}`);
+        setDetailedData(response.data);
       }
     };
 
@@ -35,12 +35,22 @@ export default function SearchBar() {
           <circle cx="14px" cy="14px" r="10px"></circle>
           <line x1="26px" y1="26px" x2="21px" y2="21px"></line>
         </svg>
-        <input
+        {/* <input
           type="text"
           name="search-bar"
           className="search-bar"
           onChange={(e) => setSearchTerm(e.target.value)}
-        />
+        /> */}
+        <select
+          className="search-bar"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        >
+          <option value=''>Select Country</option>
+          {countries.map((country) => (
+            <option key={country["ISO2"]} value={country['Slug']}>{country['Country']}</option>
+          ))}
+        </select>
       </div>
     </div>
   );
