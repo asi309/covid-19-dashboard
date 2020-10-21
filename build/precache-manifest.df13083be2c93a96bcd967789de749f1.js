ff26e3b26b1ceb0406c48a14402f57c8d69f7170
Asidipta Chaudhuri <asidiptachaudhuri@gmail.com>
2020-10-21 20:47:47 +0530

    Adds build

---
+++ build/precache-manifest.df13083be2c93a96bcd967789de749f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1799e799e7bc285fd467652f23340da2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8b304198c025e600ce8",
+    "url": "/static/css/main.90e01296.chunk.css"
+  },
+  {
+    "revision": "2d4aeaf078448b656575",
+    "url": "/static/js/2.add44a26.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.add44a26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b304198c025e600ce8",
+    "url": "/static/js/main.07d2a2fb.chunk.js"
+  },
+  {
+    "revision": "cd04e88e84a009de1bfb",
+    "url": "/static/js/runtime-main.90cbe360.js"
+  }
+]);
